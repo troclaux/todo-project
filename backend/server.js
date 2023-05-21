113a47346882be1c760c28e13fdc7a1f12217360
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-05-21 18:06:49 -0300

    feat: add server boilerplate code

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
