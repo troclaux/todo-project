7445a98a9032e0618acbb3ab33457fb6d5c352c6
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-05-21 21:15:23 -0300

    feat: add first endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,14 @@ const dotenv = require('dotenv').config();
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+
+app.get('/lists', (req, res) => {
+    res.status(200).json({
+        lists: ["list1", "list2", "list3"],
+        tasks: ["task1", "task2", "task3"]
+    });
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
