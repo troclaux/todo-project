1929bb5c466174f9940a2f71a7f85cf1106a1253
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-06-13 09:57:34 -0300

    chore: update database login credentials

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg')
  
 const pool = new Pool({
-    host: 'localhost',
-    user: 'username',
+    host: 'database-2.c6bi5vjpqcxm.sa-east-1.rds.amazonaws.com',
+    user: 'postgres',
     password: 'password',
-    database: 'db',
-})
+    database: 'db1',
+    port: 5432
+});
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
