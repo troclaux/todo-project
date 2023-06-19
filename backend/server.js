f15c2d8775e9fa12d13779eb878858430b689dee
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-06-19 16:06:28 -0300

    fix secondary port for backend

--- backend/server.js
+++ backend/server.js
@@ -91,7 +91,7 @@ app.delete('/api/v1/tasks/:id', async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
