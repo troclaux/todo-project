b118f4fedf92a07f2a3d30ea427053c2abe3a12d
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-06-21 21:06:46 -0300

    add package to enable CORS

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ const dotenv = require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const app = express();
 const db = require('./db');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 
 // Get all tasks
