0fd624423b7694eac3fda6242f04e080eec86da5
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-10-29 09:52:33 -0300

    fix: connection to rds instance

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,6 +1,20 @@
 const { Pool } = require('pg')
  
-const pool = new Pool();
+const pool = new Pool({
+    user: "postgres",
+    password: "password",
+    database: "postgres",
+    host: "database-instance.c6bi5vjpqcxm.sa-east-1.rds.amazonaws.com",
+    port: 5432,
+    ssl: true,
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+});
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
