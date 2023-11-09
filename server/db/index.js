6d3b242b8f20778ea401d641421ddf7da6c10ce2
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-11-09 15:59:07 -0300

    change settings for pool of connections

--- server/db/index.js
+++ server/db/index.js
@@ -3,19 +3,18 @@ const { Pool } = require('pg')
 const pool = new Pool({
     user: "postgres",
     password: "password",
-    database: "postgres",
-    host: "database-instance.c6bi5vjpqcxm.sa-east-1.rds.amazonaws.com",
+    host: "localhost",
     port: 5432,
-    ssl: true,
-    dialect: "postgres",
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    },
+    database: "todo"
+    // ssl: true,
+    // dialect: "postgres",
+    // dialectOptions: {
+    //     ssl: {
+    //         require: true,
+    //         rejectUnauthorized: false
+    //     }
+    // },
 });
 
-module.exports = {
-    query: (text, params) => pool.query(text, params),
-}
+module.exports = pool;
+    // query: (text, params) => pool.query(text, params),
