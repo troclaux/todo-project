8d07142c32be689a60e32b8273db2c3064a61de7
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-11-09 14:30:44 -0300

    rename api file

--- server/index.js
+++ server/index.js
@@ -110,7 +110,7 @@ app.delete('/api/v1/tasks/:id', async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
