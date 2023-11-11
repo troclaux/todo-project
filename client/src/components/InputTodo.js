f8cae347d7668833388c87d2c4c158fa0278cc89
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-11-11 17:16:09 -0300

    refresh page after creating task

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -17,6 +17,7 @@ const InputTodo = () => {
         body: JSON.stringify(body)
       });
       console.log(response);
+      window.location = "/";
     } catch (err) {
       console.error(err.message);
     }
