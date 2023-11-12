b389282fb1cdda181ce32c58e6ec151c981d2d0b
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-11-12 08:33:41 -0300

    add listTodos with delete functionality

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,35 +1,66 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 const ListTodos = () => {
+
+  const [todos, setTodos] = useState([]);
+
+  const deleteTodo = async (id) => {
+    try{
+      const deleteTodo = await fetch(`http://localhost:5000/api/v1/tasks/${id}`, {
+        method: "DELETE"
+      });
+      setTodos(todos.filter(todo => todo.id !== id));
+      console.log(deleteTodo);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/v1/tasks");
+      const jsonData = await response.json();
+      setTodos(jsonData.data.task);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  console.log(todos);
   return (
-    <fragment>
-      <table class="table">
+    <Fragment>
+      <table className="table mt-5 text-center">
         <thead>
           <tr>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>Email</th>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>John</td>
-            <td>Doe</td>
-            <td>john@example.com</td>
-          </tr>
-          <tr>
-            <td>Mary</td>
-            <td>Moe</td>
-            <td>mary@example.com</td>
-          </tr>
-          <tr>
-            <td>July</td>
-            <td>Dooley</td>
-            <td>july@example.com</td>
-          </tr>
+          {/*
+            <tr>
+              <td>John</td>
+              <td>Doe</td>
+              <td>john@example.com</td>
+            </tr>
+          */}
+          {todos.map(todo => (
+            <tr key = { todo.id }>
+              <td>{ todo.description }</td>
+              <td>Edit</td>
+              <td>
+                <button className="btn btn-danger" onClick={() => deleteTodo(todo.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
-    </fragment>
+    </Fragment>
   );
 };
 
